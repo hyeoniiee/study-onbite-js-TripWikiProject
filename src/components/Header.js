d62ad9842b5985463f3821192e421d0785f68ee5
hyeoniiee <starjea0126@gmail.com>
2024-09-27 18:57:58 +0900

    fix: day13 코드 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,38 +14,37 @@ export default function Header({
 
   this.template = () => {
     const { sortBy, searchWord } = this.state;
-    let temp = `<div class="title"><a href="/">✈️ Trip Wiki</a></div>`;
-    //if (!currentPage.includes("/city/"))
-    temp += `<div class="filter-search-container">
-            <div class="filter">
-                <select id="sortList" class="sort-list">
-                    <option value="total" ${
-                      sortBy === "total" ? "selected" : ""
-                    }>Total</option>
-                    <option value="cost" ${
-                      sortBy === "cost" ? "selected" : ""
-                    }>Cost</option>
-                    <option value="fun" ${
-                      sortBy === "fun" ? "selected" : ""
-                    }>Fun</option>
-                    <option value="safety" ${
-                      sortBy === "safety" ? "selected" : ""
-                    }>Safety</option>
-                    <option value="internet" ${
-                      sortBy === "internet" ? "selected" : ""
-                    }>Internet</option>
-                    <option value="air" ${
-                      sortBy === "air" ? "selected" : ""
-                    }>Air Quality</option>
-                    <option value="food" ${
-                      sortBy === "food" ? "selected" : ""
-                    }>Food</option>
-                </select>
-            </div>
-            <div class="search">
-            <input type="text" placeholder="Search" id="search" autocomplete="off" value=${searchWord} >
-            </div>
-        </div>`;
+    let temp = `<div class ="title">
+    <a href="/">✈️ Trip Wiki</a>
+    </div>
+    <div class ="filter-search-container">
+      <div class ="filter">
+        <select id="sortList" class = "sort-list">
+          <option value="total" ${
+            sortBy === "total" ? "selected" : ""
+          }>Total</option>
+          <option value="cost" ${
+            sortBy === "cost" ? "selected" : ""
+          }>Cost</option>
+          <option value="fun" ${sortBy === "fun" ? "selected" : ""}>Fun</option>
+          <option value="safety" ${
+            sortBy === "safety" ? "selected" : ""
+          }>Safety</option>
+          <option value="internet" ${
+            sortBy === "internet" ? "selected" : ""
+          } >Internet</option>
+          <option value="air" ${
+            sortBy === "air" ? "selected" : ""
+          }>Air Quality</option>
+          <option value="food" ${
+            sortBy === "food" ? "selected" : ""
+          }>Food</option>
+        </select>
+      </div>
+      <div class ="search">
+        <input type="text" placeholder="Search" id="search" autocomplete="off" value=${searchWord}>
+      </div>
+    </div>`;
     return temp;
   };
 
